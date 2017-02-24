209ff2b1b408f6d47e2a0f31ce2347f611706aec
Mohammed Thavaf <mthavaf@gmail.com>
2017-02-25 01:26:26 +0530

    Mqtt message i received on the Report Fragment

--- GLN/app/src/main/java/com/example/root/gln/LoggedIn.java
+++ GLN/app/src/main/java/com/example/root/gln/LoggedIn.java
@@ -1,15 +1,28 @@
 package com.example.root.gln;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class LoggedIn extends AppCompatActivity {
     private String[] drawerOptionNames;
@@ -26,13 +39,76 @@ public class LoggedIn extends AppCompatActivity {
         glnDrawerListView = (ListView) findViewById(R.id.left_drawer);
 
         glnDrawerListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drawerOptionNames));
+        glnDrawerListView.setBackgroundColor(Color.DKGRAY);
         glnDrawerListView.setOnItemClickListener(new DrawerItemClickListener());
+
+        MqttTask task = new MqttTask();
+        task.execute();
     }
 
     public class DrawerItemClickListener implements ListView.OnItemClickListener{
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             changeFragment(position);
+        }
+    }
+    public class MqttTask extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            String clientId = MqttClient.generateClientId();
+            final MqttAndroidClient client = new MqttAndroidClient(getApplicationContext(), "tcp://broker.hivemq.com:1883", clientId);
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    Log.d(Gln.TAG, "Connection Lost");
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    TextView reportSection = (TextView) findViewById(R.id.report_text);
+                    reportSection.setText(message.toString());
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    Log.d(Gln.TAG, "delivery complete");
+                }
+            });
+            try {
+                IMqttToken token = client.connect();
+
+                token.setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        try {
+                            IMqttToken subToken = client.subscribe("thavaf", 1);
+                            subToken.setActionCallback(new IMqttActionListener() {
+                                @Override
+                                public void onSuccess(IMqttToken asyncActionToken) {
+                                    Log.d(Gln.TAG, "Subscription success");
+                                }
+
+                                @Override
+                                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                                    Log.d(Gln.TAG, "Subscription failed");
+                                }
+                            });
+                        }catch (MqttException e){
+                            Log.d(Gln.TAG, e.getMessage());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.d(Gln.TAG, "connection failed");
+                    }
+                });
+            }catch (MqttException e){
+                Log.d(Gln.TAG, e.getMessage());
+            }
+
+            return null;
         }
     }
     private void changeFragment(int pos){
--- GLN/app/src/main/java/com/example/root/gln/MainActivity.java
+++ GLN/app/src/main/java/com/example/root/gln/MainActivity.java
@@ -7,11 +7,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.nispok.snackbar.Snackbar;
 
 import java.io.UnsupportedEncodingException;
 
@@ -29,14 +31,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        print("hi");
 
         loginButton = (Button) findViewById(R.id.login_button);
+
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                print("Onclick");
-                AsyncHttpClient client = new AsyncHttpClient();
+                emailInput = (EditText) findViewById(R.id.email_input);
+                passwordInput = (EditText) findViewById(R.id.password_input);
+                email = emailInput.getText().toString().trim();
+                password = passwordInput.getText().toString().trim();
+
+                /*AsyncHttpClient client = new AsyncHttpClient();
                 client.post(Gln.HOST+"/login", new RequestParams("hi","hello"), new AsyncHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
@@ -56,17 +63,17 @@ public class MainActivity extends AppCompatActivity {
                         Intent loggedIn = new Intent(MainActivity.this, LoggedIn.class);
                         startActivity(loggedIn);
                     }
-                });
+                });*/
+                if (email.equals("thavaf") && password.equals("thavaf")){
+                    Intent i = new Intent(MainActivity.this, LoggedIn.class);
+                    startActivity(i);
+                }else {
+                    android.support.design.widget.Snackbar snackbar = android.support.design.widget.Snackbar.make(v, "Wrong credentials", android.support.design.widget.Snackbar.LENGTH_SHORT);
+                    snackbar.show();
+                }
             }
         });
 
-        emailInput = (EditText) findViewById(R.id.email_input);
-        passwordInput = (EditText) findViewById(R.id.password_input);
-        email = emailInput.getText().toString().trim();
-        password = passwordInput.getText().toString().trim();
-
-        print(email);
-        print(password);
 
 
     }
