1d85fa76b3f0c0378007364f33f90e3d3913b569
Mohammed Thavaf <mthavaf@gmail.com>
2017-03-31 04:26:04 +0530

    update-numbers and login added to server

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from __future__ import  print_function
 from flask import Flask, request, Response
 from flaskext.mysql import MySQL
-import sys
 import json
 import pymysql
 
@@ -11,6 +10,7 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'CP@ijjub'
 app.config['MYSQL_DATABASE_DB'] = 'GLN'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
+
 @app.route("/")
 def hello():
     resp = Response("hello")
@@ -19,15 +19,49 @@ def hello():
     return resp
 
 
-@app.route("/login", methods=['POST', 'GET'])
+@app.route("/update-numbers", methods=['POST'])
+def update_numbers():
+    if request.method == 'POST':
+
+        unique_id = request.form['unique_id']
+        number_1 = request.form['number_1']
+        number_2 = request.form['number_2']
+        number_3 = request.form['number_3']
+        query = "UPDATE details SET ph_no_1 = '%s', ph_no_2 = '%s', ph_no_3 = '%s' WHERE id = '%s'" % (number_1, number_2, number_3, unique_id)
+
+        if len(number_1) != 13 or len(number_2) != 13 or len(number_3) != 13:
+            return json.dumps('{ "response":"error", "message":"Invalid numbers"}')
+        mysql = MySQL()
+        mysql.init_app(app)
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        cur.execute(query)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return json.dumps('{ "response":"OK", "message":"updated"}')
+
+
+@app.route("/login", methods=['POST'])
 def login():
 
-    # test login credentials here
-
     if request.method == 'POST':
-        print(request.form['a'], file=sys.stderr)
-        return json.dumps('{"message":"POST kanappa"}')
-    return json.dumps('{"name":"ashwini","mode":"silent"}')
+        user_name = request.form['user_name']
+        password = request.form['password']
+
+        mysql = MySQL()
+        mysql.init_app(app)
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        query = "SELECT id FROM details WHERE (name = '%s' AND password = '%s')" % (user_name, password)
+        cur.execute(query)
+        conn.commit()
+        returned_rows = cur.rowcount
+        cur.close()
+        conn.close()
+        if returned_rows == 1:
+            return json.dumps('{"response":"OK", "message":"%s"}' % cur.fetchone()["id"])
+        return json.dumps('{"response":"error", "message":"username or password is incorrect"}')
 
 
 @app.route("/primary-number/<unique_id>", methods=['GET'])
